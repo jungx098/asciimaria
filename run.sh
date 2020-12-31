563f703fa957df7f94a5e4e980e69d60038b1bb8
jungx098 <jungx098@gmail.com>
2020-12-30 19:33:04 -0800

    Updated size to 80x60

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
+# Ascii image size
 # printf 'TEXT 80, 49 \"'  && jp2a --size=80x49 mariapintado.jpeg && printf '\"'} | tee mariapintado.txt
-# CX=80
-# CY=49
-CX=160
-CY=100
+# CX=160
+# CY=115
 
-echo $CX
-echo $CY
+CX=80
+CY=60
+
+# Imagemagic font
+# - Available fonts can be found by `convert -list font`
+FONT='Courier'
 
 CONTENT=`jp2a --size="$CX"x"$CY" mariapintado.jpeg`
 
-# convert -size 2048x2048 xc:white -font "AndaleMono" -pointsize 16 -fill black -draw @mariapintado.txt image.png
-# convert -size 2048x2048 xc:white -font "AndaleMono" -pointsize 16 -fill black -draw @mariapintado.txt image.png
-# convert -size 2048x2048 xc:none  -box white  -pointsize 20 -gravity center \
-#           -draw "text 0,0 \"$CONTENT\"" \
-#           -trim +repage  text_special_sd.gif
+convert -size 2048x2048 xc:black -font "$FONT" -pointsize 16 -fill white \
+        -draw "text 0,0 \"$CONTENT\"" \
+        -trim +repage  asciimaria.png
 
-convert -size 2048x2048 xc:black -font "AndaleMono" -pointsize 18 -fill white \
-          -draw "text 0,0 \"$CONTENT\"" \
-          -trim +repage  asciimaria.png
+open asciimaria.png
